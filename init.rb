c1d81abd1fd004c9b4e55074e8fea495097f4180
max <mk@kernel-corp.com>
2014-07-04 17:46:06 +0700

    update init

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :centosadmin_redmine_plugin do
   name 'Centosadmin Redmine Plugin plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  author 'Kernel Wed Studio'
+  description 'This is a plugin for CentosAdmin'
+  version '0.1.0'
+  url 'https://github.com/KernelCorp/centosadmin_redmine_plugin'
+  author_url 'http://kerweb.ru/'
 end
