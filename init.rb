a07978d55e0c5f1af65cfefb950cbe7a187780d9
Max <mk@kernel-corp.com>
2014-07-09 10:37:00 +0700

    Update init.rb
    change plugin's name

--- init.rb
+++ init.rb
@@ -9,12 +9,12 @@ require 'centos_admin/patches/time_entry_patch'
 require_relative 'app/mailers/time_mailer'
 
 # Plugin
-Redmine::Plugin.register :centosadmin_redmine_plugin do
+Redmine::Plugin.register :redmine_centosadmin_timing do
   name 'Centosadmin Redmine Plugin plugin'
   author 'Kernel Wed Studio'
   description 'This is a plugin for CentosAdmin'
   version '0.1.0'
-  url 'https://github.com/KernelCorp/centosadmin_redmine_plugin'
+  url 'https://github.com/olemskoi/redmine_centosadmin_timing'
   author_url 'http://kerweb.ru/'
   settings default: { 'managers_roles' => 'Manager' }, partial: 'settings/centosadmin_redmine_settings'
 end
